3e20be4c0f07bb83e6f22bad9405e3ac8e75fa23
KanyeWestStudio <gabizinnhhhh@gmail.com>
2026-07-23 02:31:50 -0300

    Idk

--- dist/main.lua
+++ dist/main.lua
@@ -1,20 +1,5 @@
---[[
-     _      ___         ____  ______
-
-    | | /| / (_)__  ___/ / / / /  _/
-    | |/ |/ / / _ \/ _  / /_/ // /
-    |__/|__/_/_//_/\_,_/\____/___/
-
-    v1.6.65  |  2026-07-20  |  Roblox UI Library for scripts
-
-    To view the source code, see the `src/` folder on the official GitHub repository.
-
-    Author: Footagesus (Footages, .ftgs, oftgs)
-    Github: https://github.com/article-hub-studio/WindUI-Skibidi
-    Discord: https://discord.gg/ftgs-development-hub-1300692552005189632
-    License: MIT
-]]
-
+-- windui by footagesus
+-- fork by kanye west studio and article hub studio
 type ConfigType__DARKLUA_TYPE_a={
 Object:Instance,
 Camera:Instance?,
